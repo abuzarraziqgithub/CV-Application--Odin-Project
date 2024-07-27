56686563e803872f449a017f03195983f3374d11
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-07-27 13:50:27 +0500

    styling-improved

--- src/components/CVRender.jsx
+++ src/components/CVRender.jsx
@@ -3,29 +3,33 @@ import React from "react";
 function CVRender({ basicInfo, education, experiences }) {
   return (
     <>
-      <div className="text-center bg-blue-600 rounded-t-md text-gray-200 w-full h-40 flex flex-col justify-evenly">
+      <div className="text-center bg-slate-400  rounded-t-md text-gray-200 w-full h-40 flex flex-col items-center justify-evenly">
         <h1 className="text-4xl w-full">
-          <span className="text-gray-300 font-bold">{basicInfo.userName} </span>
+          <span className="text-gray-800 font-bold">{basicInfo.userName} </span>
         </h1>
-        <p className="w-full">{basicInfo.userEmail}</p>
-        <p className="w-full">{basicInfo.userPhone}</p>
+        <p className="w-full text-red-900 font-semibold">
+          {basicInfo.userEmail}
+        </p>
+        <p className=" w-36 text-gray-300 font-semibold  rounded-md bg-sky-700">
+          {basicInfo.userPhone}
+        </p>
       </div>
-      <div className=" flex flex-col items-start pl-5 pt-5 h-36">
-        <h2 className="  text-2xl font-serif font-semibold  pb-2">
+      <div className=" flex flex-col items-start pt-5 h-36 bg-slate-400">
+        <h2 className="  text-2xl font-serif font-semibold  pb-2 pl-3">
           About Me :
         </h2>
         <div
           id="bioContent"
-          className="font-medium text-zinc-800 bg-slate-300 h-36 w-full pl-3 pt-2 rounded-md"
+          className="font-medium text-zinc-800 bg-slate-300 h-36 w-full pl-3 pt-2 rounded-sm"
         >
           <h3 className="antialiased text-sm font-sans text-cyan-900">
             {basicInfo.aboutYourself}
           </h3>
         </div>
       </div>
-      <div id="education" className="flex flex-col ">
+      <div id="education" className="flex flex-col mr-5 bg-slate-400 w-full">
         <h2 className="font-medium text-3xl ml-4 mt-5 mb-3">Education:</h2>
-        <ul className="bg-slate-300  rounded-md flex flex-col items-start space-y-3 p-5 w-full ml-4">
+        <ul className="bg-slate-300  rounded-sm flex flex-col items-start space-y-3 p-4 w-full">
           <li className=" bg-red-500 text-stone-300 p-2 font-semibold rounded-md">
             {education.uniName}
           </li>
@@ -37,9 +41,9 @@ function CVRender({ basicInfo, education, experiences }) {
           </li>
         </ul>
       </div>
-      <div id="experience" className="flex flex-col ">
-        <h2 className="font-medium text-3xl ml-4 mt-5 mb-3">Job Experience:</h2>
-        <ul className="bg-slate-300  rounded-md flex flex-col items-start space-y-3 p-5 w-full ml-4">
+      <div id="experience" className="flex flex-col bg-slate-400 w-full">
+        <h2 className="font-medium text-3xl ml-4 mb-3">Job Experience:</h2>
+        <ul className="bg-slate-300  rounded-sm flex flex-col items-start space-y-3 p-5 w-full ">
           <li className=" bg-violet-800 text-stone-300 p-2 font-semibold rounded-md">
             {experiences.company}
           </li>
